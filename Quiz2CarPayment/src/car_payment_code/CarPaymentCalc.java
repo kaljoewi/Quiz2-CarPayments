3c930c4b7494d7e24d2f32e90d27334783dd0552
kalen.wilson <kalen.wilson@wifi-roaming-128-4-52-247.host.udel.edu>
2018-02-27 23:28:50 -0500

    To download zip of code

--- Quiz2CarPayment/src/car_payment_code/CarPaymentCalc.java
+++ Quiz2CarPayment/src/car_payment_code/CarPaymentCalc.java
@@ -28,3 +28,112 @@ public class CarPaymentCalc {
 	}
 
 }
+
+/* package car_payment_code;
+
+import java.lang.Math;
+
+public class CarPaymentInfo {
+	private double CarCost;
+	private double InterestRate;
+	private double DownPay;
+	private double LoanTerm;
+	
+	
+	public CarPaymentInfo (double cc, double lt, double dp, double ir) {
+		super();
+		CarCost = cc;
+		InterestRate = ir;
+		DownPay = dp;
+		LoanTerm = lt;
+	}
+
+	public double getCarCost() {
+		return CarCost;
+	}
+
+	public void setCarCost(double carCost) {
+		CarCost = carCost;
+	}
+
+	public double getInterestRate() {
+		return InterestRate;
+	}
+
+	public void setInterestRate(double interestRate) {
+		InterestRate = interestRate;
+	}
+
+	public double getDownPay() {
+		return DownPay;
+	}
+
+	public void setDownPay(double downPay) {
+		DownPay = downPay;
+	}
+
+	public double getLoanTerm() {
+		return LoanTerm;
+	}
+
+	public void setLoanTerm(double loanTerm) {
+		LoanTerm = loanTerm;
+	}
+	
+	
+	public double MonthlyPay() {
+		double interest  = (1 + (this.InterestRate/12));
+		
+		double discount = Math.pow(interest, this.LoanTerm);
+		
+		double denominator = 1 - (1 / discount);
+		
+		double subresult = ((this.InterestRate/12) / denominator);
+		
+		return this.CarCost * subresult;
+	}
+	
+	public double TotalOnInterest() {
+		double total = 0;
+		double amtloaned = this.CarCost - this.DownPay;
+		double temp = amtloaned;
+		double mpayment = this.MonthlyPay();
+		
+		for (int i = 0; i < this.LoanTerm; i++) {
+			total += temp * (this.InterestRate/12);
+			temp = temp - (mpayment - (temp * (this.InterestRate/12)));
+		}
+		return total;
+	}
+	
+}
+
+package car_payment_code;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CarPaymentInfoTest {
+
+	@Test
+	public void MonthlyTest() {
+		CarPaymentInfo car = new CarPaymentInfo(35000, 60, 0, .1);
+		if(car.MonthlyPay() < 743.65 && car.MonthlyPay() > 743) {
+			System.out.println("Works");
+		}else {
+			System.out.println(car.MonthlyPay());
+			fail("Wrong value");
+		}
+		
+		if (car.TotalOnInterest() > 9618 && car.TotalOnInterest() < 9619) {
+			System.out.print("Works too");
+		}else {
+			System.out.println(car.TotalOnInterest());
+			fail("Wrong value");
+		}
+	}
+	
+	
+}
+*/
